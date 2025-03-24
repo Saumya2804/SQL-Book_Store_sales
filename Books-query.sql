d240946f564bc259707f2c1e88c247543d7822f1
Saumya2804 <saumyajain2804@gmail.com>
2025-03-24 15:23:37 +0530

    Add files via upload

--- Books-query.sql
+++ Books-query.sql
@@ -1,4 +1,4 @@
--- -- Create Database
+\-- -- Create Database
 -- CREATE DATABASE OnlineBookstore;
 
 -- -- Switch to the database
@@ -133,16 +133,41 @@ order by count(o.order_id) desc
 limit 1;
 
 -- 5) Show the top 3 most expensive books of 'Fantasy' Genre :
-
-
+select * from books
+where genre = 'Fantasy'
+order by price desc
+limit 3;
 
 -- 6) Retrieve the total quantity of books sold by each author:
-
+select b.author, sum(o.quantity)from orders as o
+join books as b
+on b.book_id = o.book_id
+group by b.author
+order by 2 desc
 
 -- 7) List the cities where customers who spent over $30 are located:
+select distinct(c.city),o.total_amount from customers c
+join orders o
+on c.customer_id = o.customer_id
+where o.total_amount>30
+
 
 
 -- 8) Find the customer who spent the most on orders:
-
+select c.customer_id,sum(total_amount) from customers c
+join orders o 
+on c.customer_id = o.customer_id
+group by c.customer_id 
+order by sum(total_amount) desc limit 1
 
 --9) Calculate the stock remaining after fulfilling all orders:
+select b.book_id,b.stock,b.title,coalesce(sum(quantity),0) as oder_quantity ,
+	b.stock - coalesce(sum(quantity),0) AS remaining_quantity
+from books b
+left join orders o
+on b.book_id = o.book_id
+group by b.book_id
+
+
+
+
